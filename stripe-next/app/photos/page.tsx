9a2b8855047fcd92f49a254ebf2b8ddcdd562b6d
pranavbehal <pranavbehal09@gmail.com>
2024-06-24 19:23:45 -0400

    Quick Photos Update

--- stripe-next/app/photos/page.tsx
+++ stripe-next/app/photos/page.tsx
@@ -1,7 +1,14 @@
 import DownloadButton from "./DownloadButton";
 
 export default function PhotosPage() {
-  const images = ["img1.jpg", "img2.jpg", "img3.jpg"];
+  const images = [
+    "https://images.pexels.com/photos/20065715/pexels-photo-20065715/free-photo-of-the-dune-and-sea.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/20160457/pexels-photo-20160457/free-photo-of-three-people-riding-horses-on-a-bridge-at-sunset.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/48785/horse-portrait-head-halter-48785.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/1743165/pexels-photo-1743165.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/808465/pexels-photo-808465.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/818261/pexels-photo-818261.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  ];
 
   return (
     <div className="grid grid-cols-3 gap-4">
